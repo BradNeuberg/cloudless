55ba020f2e08868af854c4bf87f56120dbf1e8a6
Brad Neuberg <brad@inkling.com>
2015-08-18 16:49:37 -0700

    Match ImageNet’s keys of length 8, not 11.

--- src/cloudless/utils.py
+++ src/cloudless/utils.py
@@ -1,6 +1,6 @@
 def get_key(idx):
     """
-    Each image is a top level key with a keyname like 00000059999, in increasing
-    order starting from 00000000000.
+    Each image is a top level key with a keyname like 00059999, in increasing
+    order starting from 00000000.
     """
-    return "%011d" % (idx,)
+    return "%08d" % (idx,)
