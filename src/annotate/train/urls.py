26d95392482957c6c7caf93a0847004917b3f320
Max.Nova <max@silviaterra.com>
2015-08-20 15:59:24 -0700

    working on urls

---
+++ src/annotate/train/urls.py
@@ -0,0 +1,5 @@
+from train import views
+
+urlpatterns = [
+    url('api/getImage', views.getImage)
+]
--- src/annotate/train/views.py
+++ src/annotate/train/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def getImage(request):
+    print 'yo'
