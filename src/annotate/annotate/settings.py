9f81847870d1937e168a6f070a30634d989d8c9f
Max.Nova <max@silviaterra.com>
2015-08-20 14:48:46 -0700

    Basic database setup

--- src/annotate/annotate/settings.py
+++ src/annotate/annotate/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
+    'train'
 )
 
 MIDDLEWARE_CLASSES = (
