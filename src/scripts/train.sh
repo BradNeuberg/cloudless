f48a772b20fe13495a889e9bad8073dac27f733a
Brad Neuberg <brad@inkling.com>
2015-09-01 22:10:56 -0700

    Script to train on EC2 instance.

---
+++ src/scripts/train.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Script designed to train on a GPU EC2 instance, copy the results to S3, then potentially
+# terminate itself. Note that this script is meant to be run _on_ the EC2 instance itself.
+terminate="$1"
+if [[ $terminate == "--terminate" ]] ; then
+  echo "Terminating EC2 instance after training ends!!"
+else
+  echo "Note: We are not terminating this EC2 instance after training ends; use --terminate to terminate"
+fi
+filename=caffe-results-`date +"%m-%d-%y"`-host-`hostname`-time-`date +%s`.tar.gz
+./src/cloudless/cloudless.py -p -t -g && \
+  echo "Tarring up results..." && \
+  tar -cvzf /tmp/`echo $filename` snapshots/ logs/output0003* && \
+  echo "Sending results to S3 with filename..." && \
+  aws s3 cp /tmp/`echo $filename` s3://cloudless-data/ --region us-east-1
+if [[ $terminate == "--terminate" ]] ; then
+  echo "Cleaning up data we don't need on EBS volumes..."
+  rm -fr /data/cloudless/data/leveldb/*
+  rm -fr /data/planetlab/images/bounded/*
+  rm /data/cloudless/snapshots/*
+  shutdown -h now
+fi
+
