97618beca42b755035f105b9998e743805a2f542
Brad Neuberg <brad@inkling.com>
2015-09-01 22:11:11 -0700

    Script to copy cloudless/ repo to remote EC2 instance.

---
+++ src/scripts/aws_rsync.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Script that will rsync over the cloudless/ source to EC2. First argument is the path to your
+# EC2 keypair for this EC2 instance, while the second argument is the public DNS name for the EC2
+# instance to copy things over to. Note that this script is designed to run _outside_ of EC2 on
+# your own machine to copy resources over.
+EC2_KEYPAIR="$1"
+LOCATION="$2"
+cd .. && rsync -rave "ssh -i $EC2_KEYPAIR" -ar --exclude .git/ --exclude snapshots/ --exclude data/planetlab/images/bounded --exclude logs/ --exclude src/caffe_model/bvlc_alexnet/bvlc_alexnet_finetuned.caffemodel cloudless/ ubuntu@$LOCATION:/data/cloudless
