2de1baad96caf21e4e66f5ce5784151fe0d65bec
Brad Neuberg <brad@inkling.com>
2015-11-03 19:30:33 -0800

    Integrate in a better image selection library for annotation.

--- src/annotate/train/static/js/draw_bbox.js
+++ src/annotate/train/static/js/draw_bbox.js
@@ -21,71 +21,11 @@ function init(){
 }
 
 function on_image_load(context){
-  // Selection events will be relative to entire page; remove the influence of anything outside
-  // our image container from our drawn bounding boxes.
-  var image_offset = get_image_coords();
-
-  var x_begin, y_begin, x_end, y_end;
-
   $('.delete').on('click', on_delete);
   $('form').on('submit', on_submit);
 
-  // Piggy-back on JQuery's selectable behavior to detect selection boxes.
-  $('.image-container').selectable({
-    distance: 5,
-    filter: '#annotate-me',
-    start: function(evt){
-      // Get the mouse position when the user starts dragging.
-      x_begin = evt.pageX - image_offset.left;
-      y_begin = evt.pageY - image_offset.top;
-    },
-
-    stop: function(evt){
-      // Get the mouse position when the user stops dragging.
-      x_end = evt.pageX - image_offset.left;
-      y_end = evt.pageY - image_offset.top;
-
-      // If dragging mouse to the right, calculate the width & height.
-      var drag_left = false;
-      if ((x_end - x_begin) >= 1){
-        width = x_end - x_begin;
-        height = y_end - y_begin;
-      }
-      else {
-        // We are dragging the mouse to the left.
-        // TODO(neuberg): This doesn't seem to work consistently.
-        width = x_begin - x_end;
-        height = y_end - y_begin;
-        drag_left = true;
-      }
-
-      // TODO(neuberg): Make sure we don't go beyond any of the edges of the image while drawing
-      // the selection box.
-
-      var box =
-        $('<div></div>')
-        .addClass('new-bbox')
-        .css({
-          left: x_begin,
-          top: y_begin,
-          width: width,
-          height: height
-        })
-        .draggable({
-          containment: '#annotate-me'
-        })
-        .resizable();
-
-      // If the mouse was dragged left, offset our position.
-      if (drag_left) {
-        box.offset({
-          left: x_end,
-          top: y_begin
-        });
-      }
-
-      $('.image-container').append(box);
-    }
+  $('#annotate-me').selectAreas({
+    overlayOpacity: 0.0
   });
 }
 
@@ -95,23 +35,19 @@ function on_delete(evt){
 }
 
 function on_submit(evt){
-  new_bboxes = [];
-  $('.new-bbox')
-    .each(function(idx, entry) {
-      entry = $(entry);
-      var pos = entry.position();
-      new_bboxes.push([pos.left, pos.top, entry.width(), entry.height()]);
-    });
+  var areas = $('#annotate-me').selectAreas('relativeAreas');
 
-  new_bboxes.forEach(function(box) {
+  areas.forEach(function(box) {
     var input = $('<input type="hidden" name="new-bbox" />');
-    input.val(box.join(','));
+    input.val([box.x, box.y, box.width, box.height].join(','));
     $('form').append(input);
   });
 
   // TODO: Make sure there are either bounding boxes _or_ one of the checkboxes is selected.
 
-  // print_debug_form();
+  // Uncomment for debugging:
+  //evt.preventDefault();
+  //print_debug_form();
 }
 
 /**
@@ -128,28 +64,6 @@ function print_debug_form(){
       console.log(elem.attr('name') + '=' + elem.val());
     }
   });
-}
-
-/**
- * Calculates where our image is top, lef, width, and height in order to remove its effects from
- * the coordinates of newly drawn bounding boxes, as well as to prevent bounding boxes from being
- * dragged outside the image.
- */
-function get_image_coords(){
-  var img = $('#annotate-me');
-  var offset = img.offset();
-
-  // Coordinates can be floats on some platforms; we don't want floats to end up in our
-  // drawn bounding boxes so normalize them now.
-  var left = Math.round(offset.left);
-  var top = Math.round(offset.top);
-
-  return {
-    left: left,
-    top: top,
-    width: img.width(),
-    height: img.height()
-  };
 }
 
 $(window).ready(init);
