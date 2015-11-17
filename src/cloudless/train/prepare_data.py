bca2635bfa6de3fc5fa77d6bd3b9600fa44ee2b5
Brad Neuberg <brad@inkling.com>
2015-11-16 22:46:04 -0800

    Break prepare_data.py into its own script.
    Part of work breaking these into more modular pieces rather than a single monolithic tool.

--- src/cloudless/train/prepare_data.py
+++ src/cloudless/train/prepare_data.py
@@ -1,8 +1,12 @@
+#!/usr/bin/env python
+
+import argparse
 import shutil
 import os
 import time
 import csv
 import json
+import random
 
 from PIL import Image
 import numpy as np
@@ -11,18 +15,48 @@ from sklearn.utils import shuffle
 import plyvel
 from caffe_pb2 import Datum
 
-import constants
 import utils
 
-def prepare_data():
+def parse_command_line():
+    parser = argparse.ArgumentParser(description="""Prepares data for training via Caffe""")
+    parser.add_argument("--input_metadata", help="Path to where our bounding box metadata is",
+        type=str, default="data/planetlab/metadata/annotated.json")
+    parser.add_argument("--input_images", help="Path to where our unbounded images are",
+        type=str, default="data/planetlab/metadata")
+    parser.add_argument("--output_images", help="Path to place our cropped, bounded images",
+        type=str, default="data/planetlab/images/bounded")
+    parser.add_argument("--output_leveldb", help="Path to place our prepared leveldb directories",
+        type=str, default="data/leveldb")
+    parser.add_argument("--width", help="Width of image at training time (it will be scaled to this)",
+        type=int, default=256)
+    parser.add_argument("--height", help="Height of image at training time (it will be scaled to this)",
+        type=int, default=256)
+
+    args = vars(parser.parse_args())
+
+    if os.environ.get("CAFFE_HOME") == None:
+        print "You must set CAFFE_HOME to point to where Caffe is installed. Example:"
+        print "export CAFFE_HOME=/usr/local/caffe"
+        exit(1)
+
+    # Ensure the random number generator always starts from the same place for consistent tests.
+    random.seed(0)
+
+    prepare_data(args["input_metadata"], args["input_images"], args["output_images"],
+        args["output_leveldb"], args["width"], args["height"])
+
+def prepare_data(input_metadata, input_images, output_images, output_leveldb, width, height):
     """
     Prepares our training and validation data sets for use by Caffe.
     """
     print "Preparing data..."
 
-    print "\tParsing Planet Labs data into independent cropped bounding boxes..."
+    print "\tParsing Planet Labs data into independent cropped bounding boxes using %s..." % input_metadata
     #details = _get_landsat_details()
-    details = _crop_planetlab_images(_get_planetlab_details())
+    # TODO: When we save our cropped images, partition them into training and validation
+    # sub-directories (and make sure the set of images for each one matches what's in leveldb!)
+    # TODO: For training data (but not validation), do data augmentation here.
+    details = _crop_planetlab_images(_get_planetlab_details(input_metadata, input_images), output_images)
 
     print "\t\tClass details before balancing (balancing not implemented yet):"
     _print_input_details(details)
@@ -33,22 +67,23 @@ def prepare_data():
     (train_paths, validation_paths, train_targets, validation_targets) = _split_data_sets(details)
 
     print "\tSaving prepared data..."
-    _generate_leveldb(constants.TRAINING_FILE, train_paths, train_targets)
-    _generate_leveldb(constants.VALIDATION_FILE, validation_paths, validation_targets)
+    training_file = os.path.join(output_leveldb, "train_leveldb")
+    validation_file = os.path.join(output_leveldb, "validation_leveldb")
+    _generate_leveldb(training_file, train_paths, train_targets, width, height)
+    _generate_leveldb(validation_file, validation_paths, validation_targets, width, height)
 
-def _get_planetlab_details():
+def _get_planetlab_details(input_metadata, input_images):
     """
-    Loads available image paths and image filenames for landsat, along with any bounding boxes
+    Loads available image paths and image filenames for planetlab, along with any bounding boxes
     that might be present for clouds in them.
     """
-    print "location: %s" % constants.PLANETLAB_METADATA
+    print "Using the following metadata file: %s" % input_metadata
 
-    with open(constants.PLANETLAB_METADATA) as data_file:
+    with open(input_metadata) as data_file:
         details = json.load(data_file)
 
     for entry in details:
-        entry["image_path"] = os.path.join(constants.PLANETLAB_UNBOUNDED_IMAGES,
-            entry["image_name"])
+        entry["image_path"] = os.path.join(input_images, entry["image_name"])
         entry["target"] = 0
         if len(entry["image_annotation"]):
             entry["target"] = 1
@@ -70,35 +105,35 @@ def _get_planetlab_details():
 
     return details
 
-def _get_landsat_details():
-    """
-    Loads available image paths and image filenames for landsat, along with their target values if
-    they contain clouds or not (1 if there is a cloud, 0 otherwise).
-    """
+# def _get_landsat_details():
+#     """
+#     Loads available image paths and image filenames for landsat, along with their target values if
+#     they contain clouds or not (1 if there is a cloud, 0 otherwise).
+#     """
 
-    image_paths = []
-    targets = []
-    with open(constants.LANDSAT_METADATA, 'r') as csvfile:
-      entryreader = csv.reader(csvfile, delimiter=',', quotechar='"')
-      firstline = True
-      for row in entryreader:
-        if firstline:
-            firstline = False
-            continue
-        filename = row[0]
-        has_cloud = 0
-        if row[1] == "1":
-          has_cloud = 1
+#     image_paths = []
+#     targets = []
+#     with open(constants.LANDSAT_METADATA, 'r') as csvfile:
+#       entryreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+#       firstline = True
+#       for row in entryreader:
+#         if firstline:
+#             firstline = False
+#             continue
+#         filename = row[0]
+#         has_cloud = 0
+#         if row[1] == "1":
+#           has_cloud = 1
 
-        image_paths.append(os.path.join(constants.LANDSAT_IMAGES, filename))
-        targets.append(has_cloud)
+#         image_paths.append(os.path.join(constants.LANDSAT_IMAGES, filename))
+#         targets.append(has_cloud)
 
-    return {
-        "image_paths": image_paths,
-        "targets": targets,
-    }
+#     return {
+#         "image_paths": image_paths,
+#         "targets": targets,
+#     }
 
-def _crop_planetlab_images(details):
+def _crop_planetlab_images(details, output_images):
     """
     Generates cropped cloud and non-cloud images from our annotated bounding boxes, dumping
     them into the file system and returning their full image paths with whether they are targets
@@ -109,13 +144,13 @@ def _crop_planetlab_images(details):
     targets = []
 
     # Remove the directory to ensure we don't get old data runs included.
-    shutil.rmtree(constants.PLANETLAB_BOUNDED_IMAGES, ignore_errors=True)
-    os.makedirs(constants.PLANETLAB_BOUNDED_IMAGES)
+    shutil.rmtree(output_images, ignore_errors=True)
+    os.makedirs(output_images)
 
     for entry in details:
         if entry["target"] == 0:
             # Nothing to crop, but remove the alpha channel.
-            new_path = os.path.join(constants.PLANETLAB_BOUNDED_IMAGES, entry["image_name"])
+            new_path = os.path.join(output_images, entry["image_name"])
 
             im = Image.open(entry["image_path"])
             im = _rgba_to_rgb(im)
@@ -130,8 +165,7 @@ def _crop_planetlab_images(details):
             cloud_num = 1
             for bbox in entry["image_annotation"]:
                 try:
-                    new_path = os.path.join(constants.PLANETLAB_BOUNDED_IMAGES,
-                        "%s_cloud_%03d%s" % (root, cloud_num, ext))
+                    new_path = os.path.join(output_images, "%s_cloud_%03d%s" % (root, cloud_num, ext))
 
                     im = Image.open(entry["image_path"])
                     im = im.crop((bbox["left"], bbox["upper"], bbox["right"], bbox["lower"]))
@@ -145,9 +179,7 @@ def _crop_planetlab_images(details):
 
                     cloud_num += 1
                 except:
-                    # TODO(brad): Modify the annotation UI to not be able to produce invalid
-                    # crop values.
-                    print "\t\tInvalid crop value"
+                    print "\t\tInvalid crop value: {}".format(bbox)
 
     return {
         "image_paths": image_paths,
@@ -194,7 +226,7 @@ def _split_data_sets(details):
     return train_test_split(image_paths, targets, train_size=0.8, test_size=0.2, \
       random_state=0)
 
-def _generate_leveldb(file_path, image_paths, targets):
+def _generate_leveldb(file_path, image_paths, targets, width, height):
     """
     Caffe uses the LevelDB format to efficiently load its training and validation data; this method
     writes paired out faces in an efficient way into this format.
@@ -216,8 +248,8 @@ def _generate_leveldb(file_path, image_paths, targets):
       # Each entry in the leveldb is a Caffe protobuffer "Datum" object containing details.
       datum = Datum()
       datum.channels = 3 # RGB
-      datum.height = constants.HEIGHT
-      datum.width = constants.WIDTH
+      datum.height = height
+      datum.width = width
       # TODO: Should I swap the color channels to BGR?
       datum.data = image.tostring()
       datum.label = targets[idx]
@@ -269,3 +301,6 @@ def _rgba_to_rgb(im):
     Drops the alpha channel in an RGB image.
     """
     return im.convert('RGB')
+
+if __name__ == "__main__":
+    parse_command_line()
