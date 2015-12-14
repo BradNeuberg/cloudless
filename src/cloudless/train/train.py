ee70418240baeb46559242bd13333b12b7f34714
Brad Neuberg <brad@inkling.com>
2015-12-13 19:55:59 -0500

    Finished breaking train.py into its own independent training script.
    Will make it easier to reduce runtime dependencies when running on AWS so we don’t need a headless X Windows setup.

--- src/cloudless/train/train.py
+++ src/cloudless/train/train.py
@@ -1,63 +1,95 @@
+#!/usr/bin/env python
+
+import argparse
+import os
 import shutil
 import subprocess
 import sys
 import csv
 import fileinput
 import re
+import random
 
-import constants
-import graph
-import predict
+def parse_command_line():
+    parser = argparse.ArgumentParser(description="Trains Caffe model against prepared data")
+    parser.add_argument("--log_path", help="The path to where to place log files",
+        type=str, default="logs")
+    parser.add_argument("--log_num", help="""Number that will be appended to log files; this will
+        be automatically padded and added with zeros, such as output00001.log""", type=int,
+        default=1)
+    parser.add_argument("--solver", help="The path to our Caffe solver prototxt file",
+        type=str, default="src/caffe_model/bvlc_alexnet/solver.prototxt")
+    parser.add_argument("--input_weight_file", help="""A pre-trained Caffe model that we will use
+        to start training with in order to fine-tune from""", type=str,
+        default="src/caffe_model/bvlc_alexnet/bvlc_alexnet.caffemodel")
+    parser.add_argument("--output_weight_file", help="""Where to place the final, trained Caffe
+        model""", type=str, default="logs/latest_bvlc_alexnet_finetuned.caffemodel")
 
-def train(output_graphs, data=None, note=None):
-    print("Training data, generating graphs: %r" % output_graphs)
+    args = vars(parser.parse_args())
 
-    _run_trainer()
+    caffe_home = os.environ.get("CAFFE_HOME")
+    if caffe_home == None:
+        print "You must set CAFFE_HOME to point to where Caffe is installed. Example:"
+        print "export CAFFE_HOME=/usr/local/caffe"
+        exit(1)
 
-    _generate_parsed_logs()
-    (training_details, validation_details) = _parse_logs()
-    _move_trained_weight_file()
+    # Ensure the random number generator always starts from the same place for consistent tests.
+    random.seed(0)
 
-    if output_graphs:
-        graph.plot_results(training_details, validation_details, note)
-        predict.test_validation()
+    log_path = args["log_path"]
+    output_ending = "%04d" % (args["log_num"])
+    output_log_prefix = os.path.join(log_path, "output" + output_ending)
+    output_log_file = output_log_prefix + ".log"
 
-def _run_trainer():
+    train(caffe_home, log_path, output_log_file, args["solver"], args["input_weight_file"],
+        args["output_weight_file"])
+
+def train(caffe_home, log_path, output_log_file, solver, input_weight_file, output_weight_file):
+    """ Trains Caffe finetuning the given model. """
+    print("Training using data")
+
+    _run_trainer(caffe_home, log_path, output_log_file, solver, input_weight_file)
+
+    _generate_parsed_logs(caffe_home, log_path, output_log_file)
+    (training_details, validation_details) = _parse_logs(log_path, output_log_file)
+    _move_trained_weight_file(log_path, output_log_file, output_weight_file)
+
+    print "Finished training!"
+
+def _run_trainer(caffe_home, log_path, output_log_file, solver, input_weight_file):
     """
     Runs Caffe to train the model.
     """
     print("\tRunning trainer...")
-    with open(constants.OUTPUT_LOG_PATH, "w") as f:
-        process = subprocess.Popen([constants.CAFFE_HOME + "/build/tools/caffe", "train",
-            "--solver=" + constants.SOLVER_FILE, "--weights=" + constants.WEIGHTS_NON_FINETUNED],
+    with open(output_log_file, "w") as f:
+        process = subprocess.Popen([caffe_home + "/build/tools/caffe", "train",
+            "--solver=" + solver, "--weights=" + input_weight_file],
             stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
         for line in iter(process.stdout.readline, ''):
             sys.stdout.write(line)
             f.write(line)
 
-        print("\t\tTraining output saved to %s" % constants.OUTPUT_LOG_PATH)
+        print("\t\tTraining output saved to %s" % output_log_file)
 
-def _generate_parsed_logs():
+def _generate_parsed_logs(caffe_home, log_path, output_log_file):
     """
     Takes the raw Caffe output created while training the model in order
     to generate reduced statistics, such as giving iterations vs. test loss.
     """
 
     print("\tParsing logs...")
-    process = subprocess.Popen([constants.CAFFE_HOME + "/tools/extra/parse_log.py",
-        constants.OUTPUT_LOG_PATH, constants.LOG_DIR], stdout=subprocess.PIPE,
+    process = subprocess.Popen([caffe_home + "/tools/extra/parse_log.py",
+        output_log_file, log_path], stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT)
     for line in iter(process.stdout.readline, ''):
         sys.stdout.write(line)
 
-    shutil.rmtree(constants.OUTPUT_LOG_PATH + ".validate", ignore_errors=True)
-    shutil.move(constants.OUTPUT_LOG_PATH + ".test",
-        constants.OUTPUT_LOG_PATH + ".validate")
+    shutil.rmtree(output_log_file + ".validate", ignore_errors=True)
+    shutil.move(output_log_file + ".test", output_log_file + ".validate")
 
     # Convert the commas in the files into tabs to make them easier to read.
-    log_files = [constants.OUTPUT_LOG_PATH + ".train",
-        constants.OUTPUT_LOG_PATH + ".validate"]
+    log_files = [output_log_file + ".train", output_log_file + ".validate"]
     for line in fileinput.input(log_files, inplace=True):
         line = line.replace(u",", u"\t")
         if fileinput.isfirstline():
@@ -75,23 +107,22 @@ def _generate_parsed_logs():
     ]
     for log in logs:
         print("\n\t\tParsed %s log:" % log["title"])
-        with open(constants.OUTPUT_LOG_PATH + "." + log["filename"], "r") as f:
+        with open(output_log_file + "." + log["filename"], "r") as f:
             lines = f.read().split("\n")
             for line in lines:
                 print("\t\t\t%s" % line)
 
-    print("\t\tParsed training log saved to %s" % (constants.OUTPUT_LOG_PATH + ".train"))
-    print("\t\tParsed validation log saved to %s\n" % (constants.OUTPUT_LOG_PATH + ".validate"))
+    print("\t\tParsed training log saved to %s" % (output_log_file + ".train"))
+    print("\t\tParsed validation log saved to %s\n" % (output_log_file + ".validate"))
 
-def _parse_logs():
+def _parse_logs(log_path, output_log_file):
     """
     Parses our training and validation logs in order to return them in a way we can work with.
     """
     training_iters = []
     training_loss = []
     training_accuracy = []
-    for line in csv.reader(open(constants.OUTPUT_LOG_PATH + ".train"), delimiter="\t",
-                            skipinitialspace=True):
+    for line in csv.reader(open(output_log_file + ".train"), delimiter="\t", skipinitialspace=True):
         if re.search("Iters", str(line)):
             continue
 
@@ -102,7 +133,7 @@ def _parse_logs():
     validation_iters = []
     validation_loss = []
     validation_accuracy = []
-    for line in csv.reader(open(constants.OUTPUT_LOG_PATH + ".validate"), delimiter="\t",
+    for line in csv.reader(open(output_log_file + ".validate"), delimiter="\t",
                             skipinitialspace=True):
         if re.search("Iters", str(line)):
             continue
@@ -123,24 +154,26 @@ def _parse_logs():
         }
     )
 
-def _move_trained_weight_file():
+def _move_trained_weight_file(log_path, output_log_file, output_weight_file):
     """
-    By default Caffe doesn't change the weight file we pass in. Instead it dumps the trained model
-    over into the snapshots/ directory. This file moves the trained weight file over to a standard
-    location so we can work with it during prediction.
+    Moves our trained weight file somewhere else.
     """
-    trained_weight_file = _get_trained_weight_file()
-    print "\tMoving trained weight file from %s to %s" % \
-        (trained_weight_file, constants.WEIGHTS_FINETUNED)
-    shutil.copyfile(trained_weight_file, constants.WEIGHTS_FINETUNED)
+    trained_weight_file = _get_trained_weight_file(log_path, output_log_file)
+    print "\tMoving trained weight file from %s to %s" % (trained_weight_file, output_weight_file)
+    shutil.copyfile(trained_weight_file, output_weight_file)
 
-def _get_trained_weight_file():
+def _get_trained_weight_file(log_path, output_log_file):
     """
     Parses out the file name of the model weight file we just trained.
     """
     trained_weight_file = None
-    with open(constants.OUTPUT_LOG_PATH) as f:
+    with open(output_log_file) as f:
         content = f.read()
-        trained_weight_file = re.findall("Snapshotting to (.*)$", content, re.MULTILINE)[-1]
+        # Note: not all versions of Caffe include the phrase 'binary proto file ' in the log output.
+        trained_weight_file = re.findall("Snapshotting to (?:binary proto file )?(.*)$", content,
+                re.MULTILINE)[-1]
 
     return trained_weight_file
+
+if __name__ == "__main__":
+    parse_command_line()
